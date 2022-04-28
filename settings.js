c956b25ea1ad8479733659efcdb5c2dfe6c2c4a6
CyzerXd <100297681+CyXd404@users.noreply.github.com>
2022-04-29 06:21:30 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,14 +18,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281575886399','6281575886399','6281575886399']
-global.premium = ['6281575886399']
-global.ownernomer = '6281575886399'
-global.ownername = 'RiychDwayne'
+global.owner = ['6281382977027','6281384658505']
+global.premium = ['6281384658505']
+global.ownernomer = '6281384658505'
+global.ownername = 'CYZERSTORE'
 global.botname = 'Chika-Md'
 global.footer = 'RiychDwayne'
-global.ig = 'https://instagram.com/riychdwayne'
-global.email = 'riychh.20@gmail.com'
+global.ig = 'https://instagram.com/cyzer.store/'
+global.email = 'cyzerstore@gmail.com'
 global.region = 'Indonesia'
 global.sc = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
 global.myweb = 'https://api-riychdwayne.herokuap.com'
